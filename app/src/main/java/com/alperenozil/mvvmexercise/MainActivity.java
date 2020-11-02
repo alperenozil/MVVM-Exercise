8da9f54bdb751686f72baaf47bbe2262121f076b
necdetozil <necdet.ozil@hacettepe.edu.tr>
2020-11-02 22:41:33 +0300

    ViewModel implemented.

--- app/src/main/java/com/alperenozil/mvvmexercise/MainActivity.java
+++ app/src/main/java/com/alperenozil/mvvmexercise/MainActivity.java
@@ -1,14 +1,19 @@
 package com.alperenozil.mvvmexercise;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ViewModelProvider.AndroidViewModelFactory androidViewModelFactory=new ViewModelProvider.AndroidViewModelFactory(getApplication());
+        MainActivityViewModel model = new ViewModelProvider(this,androidViewModelFactory).get(MainActivityViewModel.class);
+        Log.d(TAG, "onCreate: "+model.getCount());
     }
 }
---
+++ app/src/main/java/com/alperenozil/mvvmexercise/MainActivityViewModel.java
@@ -0,0 +1,11 @@
+package com.alperenozil.mvvmexercise;
+
+import androidx.lifecycle.ViewModel;
+
+public class MainActivityViewModel extends ViewModel {
+    private int count=22;
+
+    public int getCount() {
+        return count;
+    }
+}
